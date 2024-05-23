e9cd908ad81ad372a99d3ce272aaa2577e127f74
drsanou <drisanou@bceao.int>
2024-05-23 15:53:29 +0000

    initial commit

---
+++ src/main/java/com/formation/demo_boot/DemoBootApplication.java
@@ -0,0 +1,20 @@
+package com.formation.demo_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBootApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello() {
+		return " Hello BCEAO V1 ";
+	}
+}
